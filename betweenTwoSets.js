72b377c96c95d44051a12e38c61bda4a7fe980fd
Thiago Jacinto Silva dos Santos <thiagojacinto@MacBook-de-Thiago.local>
2019-08-14 16:20:19 -0300

    betweenTwoSets - need to redo the bArray work

--- betweenTwoSets.js
+++ betweenTwoSets.js
@@ -11,10 +11,19 @@ function getTotalX(a, b) {
     // Write your code here
     // Defining useful variables
     let numberOfInt = 0;
+    let newBArray = [];
     let primeNumbers = [2,3,5,7,9,11,13,17,19,23]
     let bPrimeSelection = [];
     let bCopy = [];
     let aBCompatible = [];
+
+    // Putting b-array in right order
+    function compareNumbers(a, b) {
+      return a - b;
+    };
+    newBArray = b.sort(compareNumbers);
+    // - verifiy if newBArray works:
+    //console.log(newBArray);
 
     // Function to:
     // - find an array of prime factors (resultArray)
@@ -30,25 +39,24 @@ function getTotalX(a, b) {
       }
       return resultArray;
     };
-    // Function to find the multiples
-    // Returns: an array of all multiples
+    // Function to find the divisors
+    // Returns: an array of all divisors
     function findDivisorsOf(number, primeArray) {
       let rest = number;
-      let multiples = [number];
-      let item = 0
-      let prime = primeArray[0];
+      let divisors = [number];
+      let prime = 0;
       for (let k=0; k < primeArray.length; k++) {
-        //console.log(number, prime);
+        console.log(number, prime);
+        prime = primeArray[k];
         while (rest % prime == 0) {
           rest = rest/prime;
           if (rest > 1) {
-            multiples.push(rest);
+            divisors.push(rest);
           };
-          //console.log(`For this prime: ${prime}, will be this rest: ${rest}`);
+          console.log(`For this prime: ${prime}, will be this rest: ${rest}`);
         }
       }
-
-      return multiples;
+      return divisors;
     }
     // array of factors of b[0]:
     bPrimeSelection = findPrimeFactors(b[0], bPrimeSelection);
@@ -59,7 +67,7 @@ function getTotalX(a, b) {
     bCopy = bDivisors;
     for (let p=0; p<a.length; p++) {
       for (let m=0; m<bDivisors.length; m++) {
-        //console.log(`This iteration (${p},${m}), the bDivisors item is ${bDivisors[m]} and a item is ${a[p]}.`);
+        console.log(`This iteration (${p},${m}), the bDivisors item is ${bDivisors[m]} and 'a' item is ${a[p]}.`);
         if ((bDivisors[m] % a[p]) != 0) {
           bCopy = bCopy.filter(item => item != bDivisors[m])
           //console.log(bCopy)
@@ -67,16 +75,22 @@ function getTotalX(a, b) {
       }
     };
 
-    // The result will be the restant of the bDivisors:
-    aBCompatible = bCopy;
+    // The result will be the restant of the bDivisors, unless 'a' = [1]
+    if (a.includes(1)) {
+      aBCompatible = [1];
+    } else {
+      aBCompatible = bCopy;
+    }
+
+    // The final output is the number of multiples that fulfill the two constrains:
     numberOfInt = aBCompatible.length;
-    //console.log(bPrimeSelection, bDivisors, aBCompatible);
-    //console.log('* - * - * - * - * - * - * end of the system * - * - * - * - * - * - *');
+    console.log(bPrimeSelection, bDivisors, aBCompatible);
+    console.log('* - * - * - * - * - * - * end of the system * - * - * - * - * - * - *');
     return numberOfInt;
 }
 
 // Sample testing
-
+/*
 let a1 = [2,4];
 let b1 = [16,32,96];
 console.log(getTotalX(a1, b1));
@@ -84,3 +98,11 @@ console.log(getTotalX(a1, b1));
 let a2 = [3,4];
 let b2 = [24,48];
 console.log(getTotalX(a2, b2));
+
+let a3 = [1];
+let b3 = [100];
+console.log(getTotalX(a3, b3));
+*/
+let a4 = [2];
+let b4 = [20,30,12];
+console.log(getTotalX(a4, b4));
