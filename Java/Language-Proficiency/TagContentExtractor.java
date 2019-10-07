775539cf81c4d3fbd1ab57a9e1dac50e82d0b1c6
Thiago Jacinto <thiagojs.santos@gmail.com>
2019-10-07 18:31:06 -0300

    Update TagContentExtractor.java
    working fine: Inspired by BHARAT JOHRI's solution

--- Java/Language-Proficiency/TagContentExtractor.java
+++ Java/Language-Proficiency/TagContentExtractor.java
@@ -17,42 +17,19 @@ public class TagContentExtractor {
 			String line = in.nextLine();
 
 			//Write your code here
-
-			// 04-10-19: new regex to get all = (<([\w ]+)>)([\w ]+)(<\/([\w]+)>)
 			
-			// Pattern to match
-			String regexAll = "(<([\\w ]+)>)([\\w ]+)(<\\/([\\w ]+)>)";
-			// Compile the respective pattern:
-			Pattern allPattern = Pattern.compile(regexAll);
+			// Inspired by BHARAT JOHRI's solution
 			
-			String blanksBetweenTag = "(<([\\w ]+)>)()(<\\/([\\w ]+)>)";
-			Pattern blanksPattern = Pattern.compile(blanksBetweenTag);
-			
-			// Helpers variables
-//			int iteration = 0;
-			boolean moreMatches = true;
-			
-			while (moreMatches) {
-				Matcher allMatcher = allPattern.matcher(line);	
-				Matcher blanksMatcher = blanksPattern.matcher(line);
-				// Verify if 'line' has the same opening and closing tags
-//				System.out.println("OpTag: " + allMatcher.group(2)
-//				+ ", ClTag: " + allMatcher.group(5)
-//				+ ", Sentence: " + allMatcher.group(3));
-				if (allMatcher.find() && allMatcher.group(2).equals(allMatcher.group(5))) {
-					System.out.println(allMatcher.group(3));
-//					System.out.println("Line before replace: " + line);
-					line = line.replaceFirst(regexAll, "");
-//					System.out.println("Line after: " + line);
-					if (line.length() == 0 || blanksMatcher.find()) {
-						moreMatches = false;
-					}
-				} else /*if (iteration == 0)*/ {
-					// Only print if the 'line' didn't match
-					System.out.println("None");
-					moreMatches = false;
-				}
-//				iteration++;
+			String regex = "<([^>]+)>([^<>]+)</\\1>";
+			Pattern regexPattern = Pattern.compile(regex);
+			Matcher regexMatcher = regexPattern.matcher(line);
+			boolean findMatch = true;
+			while(regexMatcher.find( )){
+				System.out.println(regexMatcher.group(2));
+				findMatch = false;
+			} 
+			if(findMatch) {
+				System.out.println("None");
 			}
 			testCases--;
 		}
